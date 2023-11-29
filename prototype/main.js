c2c94b9de223e2ff55174b6d9c7c15ad20c2ec54
tashkulov <nurislamtashkulov168@gmail.com>
2023-11-29 22:43:23 +0600

    fixed

---
+++ prototype/main.js
@@ -0,0 +1,20 @@
+// let obj1 = {name: "Beka"}
+// let obj2 = obj1;
+// obj1 = {surname: "Maasaliev"}
+// console.log(obj2);
+// console.log(obj1);
+
+
+// try {
+//     console.log(1)
+// } catch (error) {
+//     console.log(2)
+
+// } finally{
+//     console.log(3)
+// }    
+
+console.log(hehe)
+console.log(gg)
+var hehe='hello'
+let gg='gegeg'
--- xoGame/ox.js
+++ xoGame/ox.js
@@ -1,25 +1,3 @@
-// const buttons=document.body.getElementsByTagName('button')
-
-// const f1 =document.getElementById('f1')
-// const s2 =document.getElementById('s2')
-// const t3 =document.getElementById('t3')
-// const f4 =document.getElementById('f4')
-// const f5 =document.getElementById('f5')
-// const s6 =document.getElementById('s6')
-// const s7 =document.getElementById('s7')
-// const e8 =document.getElementById('e8')
-// const e9 =document.getElementById('e9')
-
-// for (const button of buttons) {
-//     button.addEventListener('click', function() {
-//         this.textContent = 'X';
-//     });
-// }
-
-// if(f1.textContent=='X' && s2.textContent=='X' && t3.textContent=='X'){
-//     alert('You winn')
-// }
-
 const buttons = document.body.getElementsByTagName('button');
 
 for (const button of buttons) {
@@ -27,7 +5,12 @@ for (const button of buttons) {
         this.textContent = 'X';
 
         checkForWin();
-        placeRandomO()
+        // После каждого хода проверяем на выигрыш
+        if (!checkForWin()) {
+            placeRandomO();
+            // После каждого хода нолика снова проверяем на выигрыш
+            checkForWin();
+        }
     });
 }
 
@@ -42,43 +25,41 @@ function checkForWin() {
     const e8 = document.getElementById('e8').textContent;
     const e9 = document.getElementById('e9').textContent;
 
+    const combinations = [
+        [f1, s2, t3],
+        [f4, f5, s6],
+        [s7, e8, e9],
+        [f1, f4, s7],
+        [s2, f5, e8],
+        [t3, s6, e9],
+        [f1, f5, e9],
+        [t3, f5, s7]
+    ];
 
-    const combinations=[  
-        [f1,s2,t3],
-        [f4,f5,s6],
-        [s7,e8,e9],
-
-        [f1,f4,s7],
-        [s2,f5,e8],
-        [t3,s6,e9],
-
-        [f1,f5,e9],
-        [t3,f5,s7]
-    ]
-   
-
-    for(let combination of combinations){
-    if (combination.every(elem=>elem==='X')) {
-        alert('X win');
-        break
-    }else if(combination.every(elem=>elem==="O")){
-        alert('O win')
+    for (let combination of combinations) {
+        if (combination.every(elem => elem === 'X')) {
+            alert('X win');
+            return true;
+        } else if (combination.every(elem => elem === 'O')) {
+            alert('O win');
+            return true;
+        }
     }
 
+    return false;
 }
-}
+
 function placeRandomO() {
-
-    const forRandom = [f1, s2, t3, f4, f5, s6, s7, e8, e9];
-
+    const buttons = document.body.getElementsByTagName('button');
     let placedO = false;
 
-    for (let absrandom of forRandom) {
-        if (!placedO && absrandom.textContent !== 'X') {
-            if (Math.random() < 0.5) {
-                absrandom.textContent = 'O'
-                placedO = true
-            }
+    while (!placedO) {
+        const randomIndex = Math.floor(Math.random() * buttons.length);
+        const randomButton = buttons[randomIndex];
+
+        if (randomButton.textContent !== 'X' && randomButton.textContent !== 'O') {
+            randomButton.textContent = 'O';
+            placedO = true;
         }
     }
 }
